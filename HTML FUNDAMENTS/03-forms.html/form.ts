14f816df52ebf37fa5ad7526bdc56a64c34ce2bc
Carlos Enrique Bautista Velasco <carlosebv_cfeit_2023@outlook.com>
2024-07-17 13:31:31 -0600

    Se agrega una carpeta extra con el ejercicio y un script de js

---
+++ HTML FUNDAMENTS/03-forms.html/form.ts
@@ -0,0 +1,17 @@
+interface registroEvent{
+    name: string,
+    email: string,
+    password: string,
+    ticketType: 'general' | 'vip' | 'student'
+}
+
+const main = ():Object => {
+
+    const getData = async <registroEvent>() => {
+        return {
+            name: document.getElementById('')
+        }
+    }
+
+    return {}
+}
